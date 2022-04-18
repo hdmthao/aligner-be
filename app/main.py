87fa004919234a15be4beff7f58dfd4d494b7dee
Thao Ha <thao.ha@employmenthero.com>
2022-04-18 22:35:27 +0700

    add logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
+import logging.config
 from fastapi import FastAPI
 
+
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
