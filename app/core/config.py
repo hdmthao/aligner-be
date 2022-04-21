9f615cf9bad62fca5d0059b7e72217d3d56ba96b
Thao Ha <thao.ha@employmenthero.com>
2022-04-21 21:49:07 +0700

    add cors

--- app/core/config.py
+++ app/core/config.py
@@ -2,11 +2,14 @@ import os
 
 from dotenv import load_dotenv
 from databases import DatabaseURL
+from starlette.datastructures import CommaSeparatedStrings
 
 API_V1_STR = "/api"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 * 4 # one month
 
 load_dotenv(".env")
+
+ALLOWED_HOSTS = CommaSeparatedStrings(os.getenv("ALLOWED_HOSTS", ""))
 
 MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT", 10))
 MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
 import logging.config
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from .api.api_v1.api import router as api_router
-from .core.config import API_V1_STR
+from .core.config import API_V1_STR, ALLOWED_HOSTS
 from .database.utils import close_mongo_connection, connect_to_mongo
 
 
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 
 app = FastAPI()
+
+if not ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_HOSTS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo_connection)
